1f71ab77ab9e79ff6722d935f66950ce13e32f57
CarlosJHR64 <carlosjhr64@gmail.com>
2014-01-15 15:39:33 -0800

    edits based on ANALYSIS

--- lib/otpr/otpr.rb
+++ lib/otpr/otpr.rb
@@ -4,11 +4,10 @@ module OTPR
       unless File.exist?(yindir) and File.exist?(yangdir)
         Error.raise(:media_not_found)
       end
+      key   = DIGEST.digest(passphrase)
+      @key  = OTPR::Key.new(key)
       xcs   = CHKSUM.hexdigest(passphrase)
       wcs   = BaseConvert.new(:hexadecimal, :word).convert(xcs)
-      xdg   = DIGEST.hexdigest(passphrase)
-      qdg   = BaseConvert.new(:hexadecimal, :qgraph).convert(xdg)
-      @key  = OTPR::Key.new(qdg)
       @zin  = File.join yindir,  wcs
       @zang = File.join yangdir, wcs
     end
@@ -29,7 +28,8 @@ module OTPR
     def set(plain)
       encripted = @key.xor(plain)
       yin   = OTPR::Entropy.computer.to(:qgraph)
-      yang  = OTPR::Key.new(yin).xor(encripted)
+      dyin  = DIGEST.digest(yin)
+      yang  = OTPR::Key.new(dyin).xor(encripted)
       File.open(@zin,  'w', 0600){|f| f.write yin}
       File.open(@zang, 'w', 0600){|f| f.write yang}
     end
@@ -37,8 +37,9 @@ module OTPR
     def get
       Error.raise(:no_yin_yang) unless exist?
       yin   = File.read @zin
+      dyin  = DIGEST.digest(yin)
       yang  = File.read @zang
-      encripted = OTPR::Key.new(yin).xor(yang)
+      encripted = OTPR::Key.new(dyin).xor(yang)
       plain = @key.xor(encripted)
       return plain
     end
