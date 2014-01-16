df77c103cbdb912c078e4b6885791698379d43e3
CarlosJHR64 <carlosjhr64@gmail.com>
2014-01-16 11:51:24 -0800

    saving progress

--- lib/otpr/config.rb
+++ lib/otpr/config.rb
@@ -6,55 +6,10 @@ module OTPR
   CHKSUM  = Digest::MD5
   BASE    = 16
 
-  ACCEPT  = /^[[:graph:]]*$/
-  REJECT  = /['"]/
-
   CONFIG = {
-    # Error messages
-    :choose_one         => 'Choose one action.',
-    :writting           => 'Writting ',
-    :media_not_found    => 'Pad directories not found.',
-    :no_yin_yang        => 'Pad files not found.',
-    :not_bucket_full    => 'Did not get a full bucket.',
-    :could_not_set      => 'Could not set secret.',
-    :pin_does_not_exist => 'Pin does not exist.',
-
-    :clear         => false,
-    :clear_command => 'clear',
-    :media         => '/media/KINGSTON/',
-    :random_web    =>
-      'http://www.random.org/integers/?num=64&min=0&max=15&col=64&base=10&format=plain&rnd=new',
-
-    # About the pin
-    :enter_pin      => 'Pin: ',
-    :pin_validation => true,
-    :pin_min        => 0,
-    :pin_too_short  => "Pin is too short.",
-    :pin_max        => 64,
-    :pin_too_long   => "Pin is too long.",
-    :pin_not_valid  => "Pin has illegal characters.",
-    :repeat_pin     => "Repeat the pin.",
-
-    # About the secret
-    :enter_secret      => 'Secret: ',
-    :secret_validation => false,
-    :secret_min        => 0,
-    :secret_too_short  => "Secret is too short.",
-    :secret_max        => 64,
-    :secret_too_long   => "Secret is too long.",
-    :secret_not_valid  => "Secret has illegal characters.",
-    :repeat_secret     => "Repeat secret.",
-
-    # Prompts
-    :gibberish_prompt => 'Please randomly type at least $N words of gibberish:',
-    :confirm_delete   => 'Delete(Y/n)?: ',
-    :confirm_erase    => 'Erase All Pins(Y/n)?: ',
-
-    # What must 'yes' be on confirmations?
-    :y => 'Y',
 
     # The help text
-    :help => <<-HELP
+    :help => <<-HELP,
 Usage: otpr [options]
 Options:
   -v --version
@@ -76,11 +31,81 @@ Options:
   -l --clear      Clears screen after pin entries.
   -e --echo       # TODO: Also --no-echo.
   -g --regen      Regenerates pad.
-  -w --overwrite  Also, --no-overwrite.
+  -w --overwrite --no-overwrite.
+
+  -P --pin-validation --no-pin-validation
+  -S --secret-validation --no-secret-validation
 
   Where is the flash drive?:
   --media='/removable-media-directory/'
+
+See `gem man otpr` for more help.
     HELP
+
+    # Default Options
+    :random        => false,
+    :batch         => false,
+    :clear         => false,
+    :echo          => true,
+    :regen         => false,
+    :overwrite     => false,
+    # More default Options
+    :pin_validation    => true,
+    :secret_validation => false,
+    :media             => '/media/KINGSTON/',
+
+    # Default Options not defined in help...
+    # These keys can be accessed in ~/.config/otpr/config.json
+
+    # commands and urls
+    :clear_command => 'clear',
+    :random_web    =>
+      'http://www.random.org/integers/?num=64&min=0&max=15&col=64&base=10&format=plain&rnd=new',
+
+    # About the pin
+    :enter_pin      => 'Pin: ',
+    :pin_min        => 0,
+    :pin_too_short  => "Pin is too short.",
+    :pin_max        => 64,
+    :pin_too_long   => "Pin is too long.",
+    :pin_accept     => "^[[:graph:]]*$",
+    :pin_reject     => "['\"]",
+    :pin_not_valid  => "Pin has illegal characters.",
+    :repeat_pin     => "Repeat the pin.",
+
+    # About the secret
+    :enter_secret      => 'Secret: ',
+    :secret_min        => 0,
+    :secret_too_short  => "Secret is too short.",
+    :secret_max        => 64,
+    :secret_too_long   => "Secret is too long.",
+    :secret_accept     => "^[[:graph:]]*$",
+    :secret_reject     => "['\"]",
+    :secret_not_valid  => "Secret has illegal characters.",
+    :repeat_secret     => "Repeat secret.",
+
+    # Prompts
+    :gibberish_prompt => 'Please randomly type at least $N words of gibberish:',
+    :confirm_delete   => 'Delete(Y/n)?: ',
+    :confirm_erase    => 'Erase All Pins(Y/n)?: ',
+    # What must 'yes' be on confirmations?
+    :y => 'Y',
+
+    # Error messages
+    :choose_one         => 'Choose one action.',
+    :writting           => 'Writting ',
+    :media_not_found    => 'Pad directories not found.',
+    :no_yin_yang        => 'Pad files not found.',
+    :not_bucket_full    => 'Did not get a full bucket.',
+    :could_not_set      => 'Could not set secret.',
+    :pin_does_not_exist => 'Pin does not exist.',
+
+    # Status Responses
+    :ok           => 'OK',
+    :inconsistent => 'INCONSISTENT',
+    :not_found    => 'NOT FOUND',
+    :cancelled    => 'CANCELLED',
+
   }
 
   module Config
--- lib/otpr/pin.rb
+++ lib/otpr/pin.rb
@@ -3,16 +3,17 @@ module OTPR
     def self.gets(conf={})
       conf.extend Config
       min, max = conf[:pin_min], conf[:pin_max]
+      accept, reject = Regexp.new(conf[:pin_accept]), Regexp.new(conf[:pin_reject])
       pin, pin0 = '', nil
       # Ensure user can acurately enter and repeat the pin
       until pin == pin0
         pin, pin0, length = pin0, nil, -1
-        until length >= min and length <= max and pin0 =~ ACCEPT and !(pin0 =~ REJECT)
+        until (length >= min) and (length <= max) and (pin0 =~ accept) and !(pin0 =~ reject)
           print conf[:enter_pin]
           pin0 = STDIN.gets.strip
           length = pin0.length
           break unless conf[:pin_validation]
-          puts conf[:pin_not_valid] if !(pin0 =~ ACCEPT) or (pin0 =~ REJECT)
+          puts conf[:pin_not_valid] if !(pin0 =~ accept) or (pin0 =~ reject)
           puts conf[:pin_too_short] if length < min
           puts conf[:pin_too_long]  if length > max
         end
