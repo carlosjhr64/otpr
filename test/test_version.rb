80b20a8a2bb23a4a0127e49ceaf40ed6590ffdb2
CarlosJHR64 <carlosjhr64@gmail.com>
2014-01-23 17:59:20 -0800

    alpha version

--- test/test_version.rb
+++ test/test_version.rb
@@ -6,6 +6,6 @@ require 'otpr/version'
 
 class Test_Version < Test::Unit::TestCase
   def test_001_version
-    assert_equal '2.0.0', OTPR::VERSION
+    assert_equal '2.0.0.alpha', OTPR::VERSION
   end
 end
